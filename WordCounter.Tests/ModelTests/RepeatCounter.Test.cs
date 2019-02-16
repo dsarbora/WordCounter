6082a8a5952db4a5ffe0b72842b08f94e0063c92
Dave Sarbora <dsarbora@gmail.com>
2019-02-15 23:48:57 -0800

    finalize tests

--- WordCounter.Tests/ModelTests/RepeatCounter.Test.cs
+++ WordCounter.Tests/ModelTests/RepeatCounter.Test.cs
@@ -13,7 +13,8 @@ namespace WordCounter.Tests
             string sentence = "You can't do that.";
             string word = "Do";
             RepeatCounter testCounter = new RepeatCounter(sentence, word);
-            Assert.AreEqual (sentence, testCounter.GetSentence());
+            string sentenceLower=sentence.ToLower();
+            Assert.AreEqual (sentenceLower, testCounter.GetSentence());
         }
         [TestMethod]
         public void IsMatch_CanFindMatch_Bool()
@@ -23,9 +24,33 @@ namespace WordCounter.Tests
             RepeatCounter testCounter = new RepeatCounter(sentence, word);
             string comparison = testCounter.GetSentenceArray()[2];
             Assert.AreEqual(true, testCounter.IsMatch(comparison, word));
-
-            
         }
-
+        [TestMethod]
+        public void CheckArray_FindsAllMatches_Int()
+        {
+            string sentence = "Brown bears with brown bags.";
+            string word = "brown";
+            RepeatCounter testCounter=new RepeatCounter(sentence, word);
+            int matches=testCounter.CheckArray(testCounter.GetSentenceArray());
+            Assert.AreEqual (2, matches);
+        }
+        [TestMethod]
+        public void CheckArray_FindsOnlyMatches_()
+        {
+            string sentence = "My friends are coming over";
+            string word = "friend";
+            RepeatCounter testCounter=new RepeatCounter(sentence, word);
+            int matches=testCounter.CheckArray(testCounter.GetSentenceArray());
+            Assert.AreEqual (0, matches);
+        }
+        [TestMethod]
+        public void CheckArray_MatchesPunctuatedWords_Int()
+        {
+            string sentence = "Blue: blue, blue! Blue? Blue.";
+            string word = "blue";
+            RepeatCounter testCounter=new RepeatCounter(sentence,word);
+            int matches=testCounter.CheckArray(testCounter.GetSentenceArray());
+            Assert.AreEqual (5, matches);
+        }
     }
 }
